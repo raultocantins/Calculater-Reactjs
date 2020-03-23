b2636071b4363cfad5b21aeb329ef07ab28a073a
Alex Raul Santo Araujo <06562980143@caltins.local>
2020-03-23 14:51:29 -0300

    Calculadora desenvolvida em Reactjs e Utilizei como parâmetro a calculadora do Mac, Optei por utilizar a função Eval para fazer os calculos porquê o switch iria deixar o codigo um pouco mais complicado e o intuito deste projeto é só aprendizado.

---
+++ src/Components/Display.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import './Display.css'
+export default props =><div className="display"> {props.value}</div>
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculater from './main/Calculater'
+import './index.css'
+
+ReactDOM.render( 
+  <div>
+    <h1>Calculadora</h1>
+    <Calculater/>
+    <h3>By Alex Raul</h3>
+  </div>
+,document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
